b0445132488d087555b9c08d5b530c8962557995
GUILHERME RUFINO CAMPOS <22282090@CFP127ALUNO.LOCAL>
2023-02-10 11:23:01 -0300

    inicio

---
+++ app.js
@@ -0,0 +1,83 @@
+var readline = require('readline');
+var entradaDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+
+});
+let media = 0
+
+entradaDados.question('Digite nome do aluno\n', function(nome) {
+    let nomeAluno = nome;
+    entradaDados.question('Digite nomee do professor(a)\n', function(professor) {
+        let nomeProfessor = professor;
+        entradaDados.question("Digite sexo do aluno(a) \n", function(sexoA) {
+            let sexoAluno = sexoA;
+            entradaDados.question('Digite sexo do professor(a)\n', function(sexoP) {
+                let sexoProfessor = sexoP;
+                entradaDados.question('Digite nome do curso \n', function(curso) {
+                    let nomeCurso = curso;
+                    entradaDados.question('Digite a disciplina\n', function(disciplina) {
+                        let nomeDisciplina = disciplina;
+                        entradaDados.question('digite 1° nota \n', function(nota1) {
+                            let primeiraNota = nota1;
+                            entradaDados.question('Digite 2° nota \n', function(nota2) {
+                                let segundaNota = nota2;
+                                entradaDados.question('Digite 3° nota \n', function(nota3) {
+                                    let terceiraNota = nota3;
+                                    entradaDados.question('Digite 4/ nota \n', function(nota4) {
+                                        let quartaNota = nota4;
+
+                                        if (nomeAluno == '' || nomeProfessor == '' || sexoAluno == '' || sexoProfessor == '' || nomeCurso == '' || nomeDisciplina == '' || primeiraNota == '' || segundaNota == '' || terceiraNota == '' || quartaNota == '') {
+                                            console.log('ERRo: Não é possivel calcular sem preencher todos osd dados');
+                                        } else if (isNaN(primeiraNota) || isNaN(segundaNota) || isNaN(terceiraNota) || isNaN(quartaNota)) {
+                                            console.log('Erro:é necessario que todas as notas seja um numero,')
+                                        } else if (primeiraNota < 0 || primeiraNota > 100 || segundaNota < 0 || segundaNota > 100 || terceiraNota < 0 || terceiraNota > 100 || quartaNota < 0 || quartaNota > 100) {
+                                            console.log('ERRO: o sistema só aceita  números entre 0 e 100 ');
+
+                                        } else {
+                                            let media = (Number(primeiraNota) + Number(segundaNota) + Number(terceiraNota) + Number(quartaNota)) / 4;
+                                            console.log('sua media é : ' + media)
+                                            if (media >= 70) {
+                                                console.log('Status aprovado');
+                                            } else if (media < 50) {
+                                                console.log('Status Reprovado seu burro');
+
+                                            } else if (media >= 50 || media <= 60) {
+                                                console.log('esta de recuperacao');
+                                                entradaDados.question('qual o a nota da recuperação? \n', function(recuperacao) {
+                                                    notaRecuperacao = recuperacao;
+
+                                                    mediaRecuperacao = (Number(notaRecuperacao) + Number(media)) / 2;
+                                                    console.log(mediaRecuperacao);
+                                                    if (mediaRecuperacao >= 60) {
+                                                        console.log('aprovado na recuperação')
+                                                    } else {
+                                                        console.log('reprovado na recuperação')
+                                                    }
+
+
+
+
+                                                });
+                                            }
+
+                                        }
+                                        console.log('---------------- RELATORIO DO ALUNO ---------------------------')
+                                        console.log('o aluno ' + nomeAluno);
+                                        console.log('Curso' + nomeCurso);
+                                        console.log('Professor' + nomeProfessor);
+                                        console.log('Media final ' + media);
+                                        //console.log('media da recuperação: ' + mediaRecuperacao);
+
+
+                                    });
+                                });
+                            });
+                        });
+                    })
+                });
+            });
+        });
+
+    });
+});
---
+++ modulo/meida.js
@@ -0,0 +1,30 @@
+const calcularMedia = function(nota1, nota2, nota3, nota4) {
+    let numero1 = Number(nota1);
+    let numero2 = Number(nota2);
+    let numero3 = Number(nota3);
+    let numero4 = Number(nota4);
+    let media = 0
+
+
+
+    //  else {
+    //     let media = numero1 + numero2 + numero3 + numero4 / 4;
+    //     console.log(media)
+    //     if (media > 70) {
+    //         console.log('Status aprovado');
+    //     }
+
+    // }
+
+
+
+
+    //    let media = numero1 + numero2 + numero3 + numero4;
+
+    // console.log(media)
+    // if (media >= 70) {
+    //     console.log('status: APROVADO');
+    // }
+
+}
+console.log(calcularMedia(10, 10, 10, 10))
